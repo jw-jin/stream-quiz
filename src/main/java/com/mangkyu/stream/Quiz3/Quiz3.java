5c6d4e18b3ad432fc79909745f278723b9623b4c
우진 <woojin9812071207@gmail.com>
2024-05-12 10:54:55 +0900

    Quiz3 Complete

--- src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
+++ src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
@@ -3,6 +3,8 @@ package com.mangkyu.stream.Quiz3;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class Quiz3 {
 
@@ -12,13 +14,27 @@ public class Quiz3 {
     // 3.1 모든 숫자 쌍의 배열 리스트를 반환하여라.
     // ex) numbers1 = [1,2,3], numbers2 = [3,4] -> [(1,3), (1,4), (2,3), (2,4), (3,3), (3,4)]
     public List<Integer[]> quiz1() {
-        return Collections.emptyList();
+        return numbers1.stream()
+            .flatMap(n1 ->
+                numbers2.stream().map(n2 -> new Integer[]{n1,n2}))
+            .collect(Collectors.toList());
     }
 
     // 3.2 모든 숫자 쌍의 곱이 가장 큰 값을 반환하여라.
     // ex) numbers1 = [1,2,3], numbers2 = [3,4] -> 12
     public int quiz2() {
-        return 0;
+        Integer n1max = numbers1.stream()
+            .mapToInt(n1 -> n1)
+            .max()
+            .orElse(0);
+
+        Integer n2max = numbers2.stream()
+            .mapToInt(n2 -> n2)
+            .max()
+            .orElse(0);
+
+        return n1max * n2max;
+
     }
 
 }
